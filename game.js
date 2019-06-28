b65929232f2fdb522a3be0913839b36d7274f0b9
Hawk Shannon <hawk.dillinger@gmail.com>
2019-06-27 20:59:14 -0400

    start of Word Game

---
+++ game.js
@@ -0,0 +1,41 @@
+let words = ["sample", "example"];
+
+let currentWord = "";
+
+let currentLettersInWord = [];
+
+let underscore = 0;
+
+let underscoreArray = [];
+
+let wrongGuessed = [];
+
+let winCount = 0;
+let lossCount = 0;
+let guessCount = 9;
+
+let startGame = function(){
+  guessCount = 9;
+  currentWord = words[Math.floor(Math.random()*words.length)];
+  currentLettersInWord = currentWord.split("");
+  underscore = currentLettersInWord.length;
+  console.log(currentWord);
+  underscoreArray = [];
+  wrongGuessed = [];
+  console.log(currentLettersInWord);
+  for(let i = 0; i < underscore; i++ ){
+    underscoreArray.push("_");
+  }
+console.log(underscoreArray);
+document.getElementById("guessesLeft").innerHTML=guessCount;
+document.getElementById("wordBlank").innerHTML=underscoreArray.join(" ");
+document.getElementById("wrongGuess").innerHTML=wrongGuessed.join(" ");
+
+
+
+
+
+
+}
+ startGame();
+
