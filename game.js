5ae59ecc91dfbe55f095c176ad199fcd0d46a430
Hawk Shannon <hawk.dillinger@gmail.com>
2019-06-28 17:47:08 -0400

    styling game layout with css

--- game.js
+++ game.js
@@ -17,8 +17,11 @@ let guessCount = 9;
 
 let startGame = function(){
   guessCount = 9;
+  //choose random word to be guessed//
   currentWord = words[Math.floor(Math.random()*words.length)];
   currentLettersInWord = currentWord.split("");
+
+  //create underscore length based on random word//
   underscore = currentLettersInWord.length;
   console.log(currentWord);
   underscoreArray = [];
@@ -28,8 +31,13 @@ let startGame = function(){
     underscoreArray.push("_");
   }
 console.log(underscoreArray);
+//display number of guesses left//
 document.getElementById("guessesLeft").innerHTML=guessCount;
+
+//display user letter choosen by user//
 document.getElementById("wordBlank").innerHTML=underscoreArray.join(" ");
+
+//display users wrong letter answer//
 document.getElementById("wrongGuess").innerHTML=wrongGuessed.join(" ");
 
 
@@ -40,3 +48,14 @@ document.getElementById("wrongGuess").innerHTML=wrongGuessed.join(" ");
 }
  startGame();
 
+
+
+//get users letter guessed when key is pressed//
+
+
+
+
+})
+//check if answer is right//
+
+//tell user if answer is correct//
